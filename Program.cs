06bbe1773f77ce15fab4e58000e24dff057cb973
saji2000 <sajaddaneshmand79@gmail.com>
2025-04-03 22:11:03 -0400

    created a seperate endpoint file, praise God

--- Program.cs
+++ Program.cs
@@ -1,71 +1,7 @@
-using dotnet.Dtos;
+using dotnet.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-const string GetGameEndPoint = "GetGame";
-
-List<GameDto> games = [new (1, "Gta 6", "Open World", 59.99M, new DateOnly(2026, 10, 20)), 
-    new (2, "Gta 7", "Open World", 89.99M, new DateOnly(2046, 10, 20))];
-
-
-// GET /games
-app.MapGet("/games", () => games);
-
-// GET /games/id
-app.MapGet("/games/{id}", (int id) =>
-{
-    var index = games.FindIndex(game => game.Id == id);
-
-    if (index != -1)
-    {
-        return Results.Ok(games[index]);
-    }
-    return Results.NotFound();
-    
-}).WithName(GetGameEndPoint);
-
-// POST /games
-app.MapPost("/games", (CreateGameDto newGame) =>
-{
-    GameDto game = new (
-        games.Count + 1,
-        newGame.Name,
-        newGame.Genre,
-        newGame.Price,
-        newGame.ReleaseDate
-        );
-    
-    games.Add(game);
-
-    return Results.CreatedAtRoute(GetGameEndPoint, new { id = game.Id }, game);
-});
-
-// PUT /games/id
-app.MapPut("/games/{id}", (int id, UpdateGameDto game) =>
-{
-    var index = games.FindIndex(game => game.Id == id);
-    if (index != -1)
-    {
-        GameDto newGame = new (id, game.Name, game.Genre, game.Price, game.ReleaseDate);
-        games[index] = newGame;
-        return Results.CreatedAtRoute(GetGameEndPoint, new { id = newGame.Id }, newGame);
-    }
-
-    return Results.NotFound();
-});
-
-app.MapDelete("/games/{id}", (int id) =>
-{
-    var index = games.FindIndex(game => game.Id == id);
-
-    if (index != -1)
-    {
-        games.RemoveAt(index);
-        return Results.NoContent();
-    }
-
-    return Results.NotFound();
-});
-
+app.MapGamesEndpoints();
 app.Run();
